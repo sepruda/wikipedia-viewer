02f83ddb3cd57e524dfbb608007be908c4cce507
Christian Jess Mark <christianjessmark@gmail.com>
2018-04-30 13:07:15 +0200

    Link and hover-effect
    Cards link to articles, hover-effect

--- main.js
+++ main.js
@@ -27,17 +27,25 @@ function searchStr() {
 }
 
 function showSearch(data) {
-	console.log(data);
-	for (var i = 0 ; i < 10 ; i++) {
-		var pageTitle = data.query.search[i].title;
-		var pageSnippet = data.query.search[i].snippet;
 
-	}
-	data.query.search.forEach(element => {
+	$("#container").html(""); //tømmer resultatlisten, så nye søgninger ikke tilføjes under tidigere søgninger
+  
+
+	data.query.search.forEach(element => { //går hvert element i arrayet fra API'en igennem
+
+	      
+	      // A-taget bliver den ydre ramme for at gøre hvert kort til et link
+	      const articleLink = document.createElement('a');
+	      articleLink.setAttribute('href', 'https://en.wikipedia.org/?curid=' + element.pageid);
+	      articleLink.setAttribute('target', '_blank');
+	      articleLink.setAttribute('class', 'searchCard');
 
 	      const card = document.createElement('div');
-	      card.setAttribute('class', 'card bg-light text-dark mb-4 pl-2 pt-2', );
-	      const articleLink = 'https://en.wikipedia.org/?curid=';
+	      card.setAttribute('class', 'card bg-light text-dark mb-4 pl-2 pt-2' );
+
+	      
+	      articleLink.appendChild(card);
+	     
 
 	      const h1 = document.createElement('h5');
 	      h1.textContent = element.title;
@@ -45,14 +53,12 @@ function showSearch(data) {
 	      const p = document.createElement('p');
 	      p.innerHTML = `${element.snippet}...`;
 
-	      const link = document.createElement('a')
-	      link.innerHTML = articleLink + element.pageid;
-
-
-	      container.appendChild(card);
+	      //link.innerHTML = articleLink + element.pageid;
 	      card.appendChild(h1);
 	      card.appendChild(p);
-	      card.appendChild(link);
+
+	      container.appendChild(articleLink);
+	      
 
 	
 	 });	
